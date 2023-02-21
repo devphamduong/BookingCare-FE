9a97463e3f18cf300cbbf6c49f0b25799a5020cc
Pham Chu Duong <pduong244@gmail.com>
2023-02-21 14:29:57 +0700

    Hiển Thị Dữ Liệu Người Dùng Ra View (READ)

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,52 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-class UserManage extends Component {
+import { getAllUsers } from '../../services/userService';
+import './UserManage.scss';
 
-    state = {
+function UserManage(props) {
+    const [listUsers, setListUsers] = useState([]);
 
-    }
+    useEffect(async () => {
+        let res = await getAllUsers('ALL');
+        if (res && res.errCode === 0) {
+            setListUsers(res.users);
+        }
+    }, []);
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+    return (
+        <div className="users-container container">
+            <div className='title'>Manage users</div>
+            <div className='users-table mt-4'>
+                <table id="users">
+                    <thead>
+                        <th>Email</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        {listUsers && listUsers.length > 0 &&
+                            listUsers.map((item, index) => {
+                                return (
+                                    <tr key={`user-${index}`}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn btn-warning mx-1'><i className="fas fa-pencil-alt"></i></button>
+                                            <button className='btn btn-danger mx-1'><i className="fas fa-trash-alt"></i></button>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
-const mapStateToProps = state => {
-    return {
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
+export default UserManage;
