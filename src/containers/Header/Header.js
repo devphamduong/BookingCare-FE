5cbbe0fb1612e1af8251165e5cb983ae90a5e060
Pham Chu Duong <pduong244@gmail.com>
2023-02-28 15:23:13 +0700

    Hoàn Thiện Menu Phân Quyền Dành Cho Admin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -4,10 +4,12 @@ import { adminMenu } from './menuApp';
 import { changeLanguageApp, processLogout } from '../../store/actions';
 import { LANGUAGES } from '../../utils';
 import './Header.scss';
+import { FormattedMessage } from 'react-intl';
 
 function Header(props) {
     const dispatch = useDispatch();
     const language = useSelector(state => state.app.language);
+    const userInfo = useSelector(state => state.user.userInfo);
 
     const changeLanguage = (locale) => {
         dispatch(changeLanguageApp(locale));
@@ -20,6 +22,7 @@ function Header(props) {
                 <Navigator menus={adminMenu} />
             </div>
             <div className='languages'>
+                <span className='welcome'><FormattedMessage id='home-header.welcome' />{userInfo && userInfo.firstName ? userInfo.firstName : ''}!</span>
                 <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => changeLanguage(LANGUAGES.VI)}>VN</span></div>
                 <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => changeLanguage(LANGUAGES.EN)}>EN</span></div>
                 {/* nút logout */}
--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -7,7 +7,7 @@ function About(props) {
             </div>
             <div className="section-about-content">
                 <div className="content-left">
-                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/INzGi5v1ypQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/INzGi5v1ypQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                 </div>
                 <div className="content-right">
                     <p>You are the<strong>CSS</strong> to my <strong>HTML</strong></p>
