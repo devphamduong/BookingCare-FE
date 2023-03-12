b69e72cf358232cc12d68b384ab6060338a4fc22
Pham Chu Duong <pduong244@gmail.com>
2023-03-12 21:33:25 +0700

    Load Động Data Chọn Lịch Khám Bệnh Theo Thời Gian Ngày Tạo

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -9,6 +9,7 @@ import { getScheduleByDate } from '../../../services/userService';
 function DoctorSchedule(props) {
     const language = useSelector(state => state.app.language);
     const [allDays, setAllDays] = useState([]);
+    const [allAvailableTime, setAllAvailableTime] = useState([]);
 
     useEffect(() => {
         getAllDate();
@@ -23,7 +24,7 @@ function DoctorSchedule(props) {
         for (let i = 0; i < 7; i++) {
             let obj = {};
             if (language === LANGUAGES.VI) {
-                obj.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM').replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+                obj.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM').replace(/(^\w{1})/g, letter => letter.toUpperCase());
             } else {
                 obj.label = moment(new Date()).add(i, 'days').locale('en').format('ddd - DD/MM');
             }
@@ -39,7 +40,7 @@ function DoctorSchedule(props) {
             let date = event.target.value;
             let res = await getScheduleByDate(doctorId, date);
             if (res && res.errCode === 0) {
-                console.log(res);
+                setAllAvailableTime(res.data ? res.data : []);
             }
         }
     };
@@ -58,7 +59,21 @@ function DoctorSchedule(props) {
                 </select>
             </div>
             <div className='all-available-time'>
-
+                <div className='text-calendar'>
+                    <i className="fas fa-calendar-alt"><span> Lịch khám</span></i>
+                </div>
+                <div className='time-content'>
+                    {allAvailableTime && allAvailableTime.length > 0 ?
+                        allAvailableTime.map((item, index) => {
+                            let time = language === LANGUAGES.VI ? item.timeTypeData.valueVi : item.timeTypeData.valueEn;
+                            return (
+                                <button key={index}>{time}</button>
+                            );
+                        })
+                        :
+                        <div>Không có lịch hẹn trong thời gian này, vui lòng chọn thời gian khác!</div>
+                    }
+                </div>
             </div>
         </div>
     );
