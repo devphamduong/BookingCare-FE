20ad09025f68f2fa4839cf41d4c1334f476b2dc3
Pham Chu Duong <pduong244@gmail.com>
2023-03-23 10:18:46 +0700

    Design Giao Diện Chức Năng Xem Chi Tiết Chuyên Khoa

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import Header from '../../HomePage/Header';
+import DoctorSchedule from '../Doctor/DoctorSchedule';
+import DoctorExtraInfor from '../Doctor/DoctorExtraInfor';
+import ProfileDoctor from '../Doctor/ProfileDoctor';
 import './DetailSpecialty.scss';
 
 function DetailSpecialty(props) {
+    const [arrDoctorId, setArrDoctorId] = useState([13, 14]);
 
     return (
-        <>
+        <div className='detail-specialty-container'>
             <Header />
-            abc
-        </>
+            <div className='detail-specialty-body'>
+                <div className='detail-specialty-description'>
+
+                </div>
+                {arrDoctorId && arrDoctorId.length > 0 &&
+                    arrDoctorId.map((item, index) => {
+                        return (
+                            <div className='each-doctor' key={index}>
+                                <div className='detail-content-left'>
+                                    <div className='profile-doctor'>
+                                        <ProfileDoctor isShowDescription={true} doctorId={item} />
+                                    </div>
+                                </div>
+                                <div className='detail-content-right'>
+                                    <div className='doctor-schedule'>
+                                        <DoctorSchedule doctorId={item} />
+                                    </div>
+                                    <div className='doctor-extra-infor'>
+                                        <DoctorExtraInfor doctorId={item} />
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </div>
     );
 }
 
